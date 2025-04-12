3b98f8730351e5748d6274e3bcf27d3818e737fe
RahulShrivastava01 <rahul.shrivastava01@outlook.com>
2025-04-12 12:31:52 +0530

    Commit.

---
+++ src/main/java/com/local/spring_docker_db_app/SpringDockerDbAppApplication.java
@@ -0,0 +1,13 @@
+package com.local.spring_docker_db_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerDbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerDbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/local/spring_docker_db_app/SpringDockerDbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.spring_docker_db_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerDbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
